eb0351443863cbae701e705174ac3e2a48264b0a
GIT-RAUNAK <raunak2808.business@gmail.com>
2026-05-13 16:12:34 +0530

    Bubble Sort

---
+++ 4. Sorting/P2.cpp
@@ -0,0 +1,26 @@
+// Bubble Sort
+
+#include<iostream>
+using namespace std;
+#include<vector>
+
+int main(){
+    int n;
+    cout<<"Enter the size of array:\n";
+    cin>>n;
+    vector<int> arr(n);
+    cout<<"Enter the elements in the array:\n";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]) 
+                swap(arr[j],arr[j+1]);
+        }
+    }
+    cout<<"Sorted array is:\n";
+    for(int x: arr){
+        cout<<x<<" ";
+    }
+}
