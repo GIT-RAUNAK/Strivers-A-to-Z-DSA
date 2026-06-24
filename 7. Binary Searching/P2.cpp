03229ffd9f4981abff731e10c5cf396904aff87c
GIT-RAUNAK <raunak2808.business@gmail.com>
2026-06-24 19:52:55 +0530

    Implement Lower Bound

---
+++ 7. Binary Searching/P2.cpp
@@ -0,0 +1,37 @@
+// Implement Lower Bound
+
+#include<iostream>
+using namespace std;
+#include<vector>
+
+int lowerBound(vector<int>& arr, int target) {
+    int n=arr.size();
+    int low=0, high=n-1;
+    int ans=n;
+    while(low<=high){
+        int mid=low+(high-low)/2;
+        if(arr[mid]>=target){
+            ans=mid;
+            high=mid-1;
+        }
+        else{
+            low=mid+1;
+        }
+    }
+    return ans;
+}
+
+int main(){
+    int n;
+    cout<<"Enter size of the array:\n";
+    cin>>n;
+    vector<int> arr(n);
+    cout<<"Enter element in the array:\n";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int target;
+    cout<<"Enter target element:\n";
+    cin>>target;
+    cout<<"Target element is at position: "<<lowerBound(arr,target);
+}
