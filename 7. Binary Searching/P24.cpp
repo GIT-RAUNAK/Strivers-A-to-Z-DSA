364c017a38b71add4eac0e43984ff551c5e4e371
GIT-RAUNAK <raunak2808.business@gmail.com>
2026-07-13 12:45:07 +0530

    Split Array Largest Sum

---
+++ 7. Binary Searching/P24.cpp
@@ -0,0 +1,52 @@
+// Split array - Largest Sum
+
+#include<iostream>
+using namespace std;
+#include<vector>
+#include<algorithm>
+
+bool canSplit(vector<int>& arr, int mid, int k){
+    int countArray=1;
+    int sum=arr[0];
+    for(int i=1;i<arr.size();i++){
+        sum+=arr[i];
+        if(sum>mid) {
+            countArray++;
+            sum=arr[i];
+        }
+    }
+    if(countArray<=k) return true;
+    else return false;
+}
+
+int splitArray(vector<int> &arr, int k){
+    int n=arr.size();
+    int low=*max_element(arr.begin(),arr.end());
+    int high=0;
+    for(int x : arr) high+=x;
+    int val=-1;
+    while(low<=high){
+        int mid=low+(high-low)/2;
+        if(canSplit(arr, mid, k)){
+            val=mid;
+            high=mid-1;
+        }
+        else low=mid+1;
+    }
+    return val;
+}
+
+int main(){
+    int n;
+    cout<<"Enter the size of the array:\n";
+    cin>>n;
+    vector<int> arr(n);
+    cout<<"Enter elements in the array:\n";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int k;
+    cout<<"Enter toal number of required subarrays:\n";
+    cin>>k;
+    cout<<"The minimum larget sum is: "<<splitArray(arr,k);
+}
