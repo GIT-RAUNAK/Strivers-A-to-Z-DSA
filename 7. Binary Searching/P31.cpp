7cd63ac6b45ac5f024ce5864a4ad2354fd2c9108
GIT-RAUNAK <raunak2808.business@gmail.com>
2026-07-17 13:24:21 +0530

    Search a 2D Matrix II

--- 7. Binary Searching/P31.cpp
+++ 7. Binary Searching/P31.cpp
@@ -11,8 +11,8 @@ bool searchMatrix(vector<vector<int>>& matrix, int target) {
     int j=cols-1;
     while(i<rows && j>=0){
         if(matrix[i][j]==target) return true;
-        else if(matrix[i][j]>target) cols--;
-        else rows++;
+        else if(matrix[i][j]>target) j--;
+        else i++;
     }
     return false;
 }
